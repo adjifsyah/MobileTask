2109adb8f1220f2d22b63b96ed50fd83e0895d70
adjifsyah <adji.firmansyah27@gmail.com>
2025-08-21 15:17:03 +0700

    add project

---
+++ MobileTask/MobileTask/MobileTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileTaskApp.swift
+//  MobileTask
+//
+//  Created by Adji Firmansyah on 21/08/25.
+//
+
+import SwiftUI
+
+@main
+struct MobileTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
